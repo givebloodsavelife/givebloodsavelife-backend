bcbb763f725e1ecdb9a75d64975f0509ad8d53d5
Kalyan Mazumder <kalyaninform@gmail.com>
2020-03-12 14:48:38 +0530

    initial project setup

---
+++ gbslapp-backend/src/main/java/org/givebloodsavelife/apps/gbslappbackend/GbslappBackendApplication.java
@@ -0,0 +1,13 @@
+package org.givebloodsavelife.apps.gbslappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbslappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbslappBackendApplication.class, args);
+	}
+
+}
---
+++ gbslapp-backend/src/test/java/org/givebloodsavelife/apps/gbslappbackend/GbslappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.givebloodsavelife.apps.gbslappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbslappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
